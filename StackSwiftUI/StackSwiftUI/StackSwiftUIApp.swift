0a3fe6b6c322facede2cd4ecf9d8d797a30654aa
Karamjit Singh <karamjitsingh1989@gmail.com>
2023-01-18 07:44:16 +0530

    VStack and HStack swiftUI

---
+++ StackSwiftUI/StackSwiftUI/StackSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackSwiftUIApp.swift
+//  StackSwiftUI
+//
+//  Created by Harsimrat on 18/01/23.
+//
+
+import SwiftUI
+
+@main
+struct StackSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
